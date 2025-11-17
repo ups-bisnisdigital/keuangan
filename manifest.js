82f975069702c2c76ba1e6fe38e05f6772bffaea
inu <ibnutaufikbalya@gmail.com>
2025-11-17 23:59:35 +0700

    Create manifest.js

---
+++ manifest.js
@@ -0,0 +1,10 @@
+{
+  "name": "Analisa Harga Bawang Merah",
+  "short_name": "BawangAnalytics",
+  "description": "Analisis harga bawang merah berdasarkan parameter lokal",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#f5f7f9",
+  "theme_color": "#4caf50",
+  "orientation": "portrait"
+}
